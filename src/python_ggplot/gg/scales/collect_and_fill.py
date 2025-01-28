4ea674c57e11a8858840c842beba68395b02ef5c
charalmapos papaloizou <harry@e2e4.email>
2025-01-28 19:26:25 +0000

    finish continuous collect and fill

--- src/python_ggplot/gg/scales/collect_and_fill.py
+++ src/python_ggplot/gg/scales/collect_and_fill.py
@@ -1,12 +1,13 @@
 from collections import OrderedDict
-from typing import Any, List, Optional, Tuple
+from typing import Any, Dict, List, Literal, Optional, Tuple
 
 import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
+from typing_extensions import Union
 
 from python_ggplot.colormaps.color_maps import int_to_color
-from python_ggplot.core.objects import ColorHCL, LineType, MarkerKind, Scale
+from python_ggplot.core.objects import AxisKind, ColorHCL, LineType, MarkerKind, Scale
 from python_ggplot.gg.datamancer_pandas_compat import (  # FormulaType,; ScalarFormula,; pandas_series_to_column,
     FormulaNode,
     GGValue,
@@ -25,7 +26,20 @@ from python_ggplot.gg.scales import (
     ShapeScale,
     SizeScale,
 )
-from python_ggplot.gg.scales.base import GGScaleData, ScaleType, scale_type_to_cls
+from python_ggplot.gg.scales.base import (
+    ColorScale,
+    ColorScaleKind,
+    DateScale,
+    FillColorScale,
+    GGScaleContinuous,
+    GGScaleData,
+    LinearAndTransformScaleData,
+    LinearDataScale,
+    ScaleTransform,
+    ScaleType,
+    TransformedDataScale,
+    scale_type_to_cls,
+)
 from python_ggplot.gg.scales.values import (
     AlphaScaleValue,
     ColorScaleValue,
@@ -274,4 +288,227 @@ def fill_discrete_shape_scale(
         discrete_kind=discrete_kind,
     )
     result = ShapeScale(gg_data=gg_data)
+    return result
+
+
+def fill_continuous_linear_scale(
+    col: FormulaNode, ax_kind: AxisKind, v_kind: GGValue, data_scale: Scale
+):
+    discrete_kind = GGScaleContinuous(
+        data_scale=data_scale,
+    )
+
+    result = LinearDataScale(
+        gg_data=GGScaleData(
+            col=col,
+            ids=set(),  # type: ignore
+            value_kind=v_kind,
+            has_discreteness=True,
+            discrete_kind=discrete_kind,
+        ),
+    )
+    return result
+
+
+def fill_continuous_transformed_scale(
+    col: FormulaNode,
+    axis_kind: AxisKind,
+    value_kind: GGValue,
+    trans: ScaleTransform,
+    inv_trans: ScaleTransform,
+    data_scale: Scale,
+):
+
+    gg_data = GGScaleData(
+        col=col,
+        ids=set(),  # type: ignore
+        value_kind=value_kind,
+        has_discreteness=True,
+        discrete_kind=GGScaleContinuous(
+            data_scale=Scale(low=trans(data_scale.low), high=trans(data_scale.high)),
+        ),
+    )
+    result = TransformedDataScale(
+        gg_data=gg_data,
+        data=LinearAndTransformScaleData(
+            axis_kind=axis_kind,
+            reversed=False,
+            transform=trans,
+        ),
+    )
+    return result
+
+
+def fill_continuous_color_scale(
+    scale_type: Union[Literal[ScaleType.COLOR], Literal[ScaleType.FILL_COLOR]],
+    col: FormulaNode,
+    data_kind: DataType,
+    value_kind: GGValue,
+    data_scale: Scale,
+    color_scale: ColorScale,
+) -> Union[ColorScaleKind, FillColorScale]:
+    cls = scale_type_to_cls(scale_type)
+    if not isinstance(cls, (ColorScaleKind, FillColorScale)):
+        raise GGException("expected color or fill color scale")
+
+    gg_data = GGScaleData(
+        col=col,
+        ids=set(),  # type: ignore
+        value_kind=value_kind,
+        has_discreteness=False,
+        discrete_kind=GGScaleContinuous(
+            data_scale=data_scale,
+        ),
+    )
+
+    result = cls(  # type: ignore TODO make a color mixin later
+        gg_data=gg_data, color_scale=color_scale
+    )
+
+    def map_data(df: pd.DataFrame) -> List[Any]:
+        # TODO FIX THIS
+        result: List[Any] = []
+        t_col = col.evaluate(df)
+        scale_val: Dict[Any, Any] = {}
+        if scale_type == ScaleType.COLOR:
+            scale_val = {"kind": "color"}
+        else:
+            scale_val = {"kind": "fill_color"}
+
+        if data_kind == DataType.MAPPING:
+            t = t_col.to_numpy(dtype=float)
+            if len(t) != len(df):
+                raise GGException("Resulting array size does not match df len!")
+
+            for val in t:
+                color_idx = int(
+                    round(
+                        255.0
+                        * ((val - data_scale.low) / (data_scale.high - data_scale.low))
+                    )
+                )
+                color_idx = max(0, min(255, color_idx))
+                c_val = color_scale.colors[color_idx]
+                scale_val["color"] = int_to_color(c_val)
+                result.append(scale_val.copy())
+        elif data_kind == DataType.SETTING:
+            t = t_col.to_numpy()
+            if len(t) != len(df):
+                raise GGException("Resulting array size does not match df len!")
+
+            if np.issubdtype(t.dtype, np.integer) or np.issubdtype(t.dtype, np.str_):
+                for val in t:
+                    scale_val["color"] = val.to_color()
+                    result.append(scale_val.copy())
+            else:
+                raise ValueError(
+                    f"Invalid column type {t.dtype} of column {col} to set a color!"
+                )
+        else:
+            raise GGException("expected mapping or setting for data type")
+        return result
+
+    result.map_data = map_data
+    return result  # type: ignore
+
+
+def fill_continuous_size_scale(
+    col: FormulaNode,
+    data_kind: DataType,
+    value_kind: GGValue,
+    data_scale: Scale,
+    size_range: Tuple[float, float],
+) -> SizeScale:
+    """
+    TODO refactor/reuse
+    """
+    if size_range[0] == size_range[1]:
+        raise GGException("Size range must be defined in this context!")
+
+    min_size = size_range[0]
+    max_size = size_range[1]
+
+    gg_data = GGScaleData(
+        col=col,
+        ids=set(),  # type: ignore
+        value_kind=value_kind,
+        has_discreteness=False,
+        discrete_kind=GGScaleContinuous(
+            data_scale=data_scale,
+        ),
+    )
+    result = SizeScale(
+        gg_data=gg_data,
+        size=SizeScaleValue(),
+        size_range=size_range,
+    )
+
+    def map_data(df: pd.DataFrame):
+        result: List[Any] = []
+        t = col.evaluate(df).to_numpy(dtype=float)
+        if len(t) != len(df):
+            raise GGException("Resulting array size does not match df len!")
+
+        for val in t:
+            if data_kind == DataType.MAPPING:
+                size = (val - min_size) / (max_size - min_size)
+            elif data_kind == DataType.SETTING:
+                size = val
+            else:
+                raise GGException("incorrect mapping type")
+            result.append({"kind": "size", "size": size})
+
+        return result
+
+    result.map_data = map_data  # type: ignore
+    return result
+
+
+def fill_continuous_alpha_scale(
+    col: FormulaNode,
+    data_kind: DataType,
+    value_kind: GGValue,
+    data_scale: Scale,
+    alpha_range: Tuple[float, float],
+) -> AlphaScale:
+    """
+    TODO refactor/reuse
+    """
+    if alpha_range[0] == alpha_range[1]:
+        raise GGException("Size range must be defined in this context!")
+
+    min_alpha = alpha_range[0]
+    max_alpha = alpha_range[1]
+
+    gg_data = GGScaleData(
+        col=col,
+        ids=set(),  # type: ignore
+        value_kind=value_kind,
+        has_discreteness=False,
+        discrete_kind=GGScaleContinuous(
+            data_scale=data_scale,
+        ),
+    )
+    result = AlphaScale(
+        gg_data=gg_data,
+        alpha=0.0,  # TODO we initialise we fake value at start, after the cleanup/refactor rethink
+    )
+
+    def map_data(df: pd.DataFrame):
+        t = col.evaluate(df).to_numpy(dtype=float)
+        result: List[Any] = []
+        alpha = 0.0
+        assert len(t) == len(df), "Resulting tensor size does not match df len!"
+
+        for val in t:
+            if data_kind == DataType.MAPPING:
+                alpha = (val - min_alpha) / (max_alpha - min_alpha)
+            elif data_kind == DataType.SETTING:
+                alpha = val
+            else:
+                raise GGException("incorrect mapping type")
+            result.append({"kind": "alpha", "alpha": alpha})
+        return result
+
+    result.map_data = map_data  # type: ignore
     return result
